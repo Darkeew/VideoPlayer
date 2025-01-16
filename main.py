1db782a46b607c1742dbb21510fb388b4020daa2
Darkeew <83980195+Darkeew@users.noreply.github.com>
2025-01-16 11:32:18 +0000

    go go gadget carbomb

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import os
 from pytube import Playlist
 import json
 import random
+import time
 
 class VODs:
     def __init__(self):
         self.player = vlc.Instance()
         self.media_player = self.player.media_player_new()
-        self.ydl_opts = {'outtmpl':'VODs/video.mp4', 'format':"best[ext=mp4]"}
-        self.url = "https://www.youtube.com/watch?v=D8T7vBBNkT4&list=PL3HIihraebSjjFa_ow3WoEKseRmu1Z5k6"
+        self.ydl_opts = {'outtmpl':'VODs/recent_%(title)s.%(ext)s', 'format':"best[ext=mp4]"}
+        self.url = "https://www.youtube.com/watch?v=-9D3b3G-2YQ&list=PLyuakwzSUr6ARBtMZkYsde8TWgfn9olm7"
         self.video_queue = None
 
     def get_playlist(self):
@@ -45,51 +46,63 @@ class VODs:
         random_n = random.randint(0 , array_length)
         return vods_array[random_n]
 
-    def get_video(self, skip = None):
-        #Check for videos in the folder, this is ignored if `skip` is true.
-        if not skip:
-            for f in os.listdir('VODs'):
-                if ".mp4" in f or ".part" in f:
-                    os.remove(f"VODs\\{f}")
+    def get_video(self, skip=None):
         #Get the URL to download
-        url = self.get_url()
-        self.video_queue = url
-        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
-            ydl.download([url])
-        return "VODs\\video.mp4"
+        if not skip:
+            url = self.get_url()
+            self.video_queue = url
+            with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+                ydl.download([url])
+        #Get the name of the file
+        for f in os.listdir("VODs"):
+            if f.startswith("recent"):
+                return f
 
-    def set_vod(self):
-        #Transform the video into a VLC media object
-        video = self.get_video()
-        os.rename(video, "VODs\\playing.mp4")
-        media = vlc.Media(video)
-        #Add to the media player
+    def set_vod(self, skip=None):
+        #If skip is `true`, dont download video
+        if not skip:
+            video = self.get_video()
+        else:
+            video = self.get_video(skip=skip)
+        new_video = video.replace("recent_", "")
+        os.rename(f"VODs\\{video}", f"VODs\\{new_video}")
+
+        media = vlc.Media(f"VODs\\{new_video}")
         self.media_player.set_media(media)
 
     def start_player(self):
         while True:
             if self.media_player.is_playing() == 0:
                 #Set VOD and start player
-                self.set_vod()
-                self.media_player.play()
+                if not self.video_queue:
+                    self.set_vod()
+                else:
+                    self.set_vod(skip=True)
                 #Remove last video played
-                with open("VODs\\vods.json", "r+") as vj:
+                with open("VODs\\vods.json", "r") as vj:
                     vods_data = json.load(vj)
                     vods_array = vods_data["vods"]
                     vods_array.remove(self.video_queue)
                     data = {"vods": vods_array}
-                    json.dump(data, vj)
+                    with open("VODs\\vods.json", "w") as vj:
+                        json.dump(data, vj)
                 #Add to watched list
                 with open("VODs\\watched.json", "r") as wj:
                     watched_data = json.load(wj)
                     watched_array = watched_data['watched']
                     watched_array.append(self.video_queue)
                     data = {"watched": watched_array}
-                    json.dump(data, wj)
+                    with open("VODs\\watched.json", "w") as wj:
+                        json.dump(data, wj)
+                    time.sleep(1)
+                    self.media_player.play()
                 self.video_queue = None
+                time.sleep(0.1)
             elif not self.video_queue:
-                self.get_video(self, skip=True)
+                self.get_video()
+
 
             
 vods = VODs()
+vods.get_playlist()
 vods.start_player()
